6dfcfdd4393a75d8386a6db3dd81b960ed7425ee
LWanjiru <linnette.wanjiru@andela.com>
2017-08-23 17:03:28 +0300

    implement hot-loading

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <h1>Hello from ray's react</h1>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello from ray's react</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,18 +1,26 @@
 const path = require('path');
+const webpack = require('webpack');
 
 export default {
-  devtools: 'eval-source-map',
-  entry : path.join(__dirname, '/client/index.js'),
+  entry : [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')],
   output: {
     filename: 'bundle.js',
     publicPath: '/',
   },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin,
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module:{
     loaders: [
       {
         test: /\.js$/,
         include: path.join(__dirname, 'client'),
-        loaders: ['babel-loader']
+        loaders: ['react-hot-loader', 'babel-loader'],
+        
       }
     ],
   }
